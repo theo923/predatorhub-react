320b2496edc3ff9f1b373da13ef638bcae819fb8
theo923 <theotam912@gmail.com>
2023-06-10 18:01:40 -0600

    vite compile fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/predatorhub-react/",
+  base: "./",
   test: {
     globals: true,
     environment: "jsdom",
